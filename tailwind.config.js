564571a689e3f95d9842b8341b31509a06a1ed20
remotsde <remot@gsm.sale>
2024-11-15 01:14:39 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      screens: {
+        'xs': '475px',
+        'print': {'raw': 'print'},
+      },
+      spacing: {
+        '18': '4.5rem',
+        '112': '28rem',
+        '128': '32rem',
+      },
+    },
+  },
+  plugins: [],
+};
